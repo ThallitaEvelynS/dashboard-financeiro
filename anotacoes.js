bf58aa57c6c0226e8f7b5b4c8587da80c16621ec
Thalita <evelynthalita55@gmail.com>
2026-08-30 15:31:33 -0300

    validação de erros de senha e email

--- anotacoes.js
+++ anotacoes.js
@@ -6,6 +6,16 @@ function validarCampos(){
     verificar se a senha é válida chamando descobreSeSenhaValida()
     habilitar entrar somente se email e senha forem válidos
 }
+--------------------------------------------------------------------------------------
+function validarCampos(){
+    const emailValido = descobreSeEmailValido();
+    document.getElementById('recover-password-button').disabled = !emailValido;
+
+    const senhaValida = descobreSeSenhaValida();
+    document.getElementById("login-button").disabled = !emailValido || !senhaValida;
+}
+---------------------------------------------------------------------------------------
+
 
 function descobreSeEmailValido() {
     pegar o valor do campo de email
@@ -29,5 +39,96 @@ function validarEmail(email) {
     se seguir esse formato, retornar verdadeiro
     se não seguir, retornar falso
 }
+
+function alternarErrosEmail() {
+    pegar o valor do campo de email
+    verificar se o campo de email está vazio
+    se estiver vazio, mostrar o erro "E-mail é obrigatório"
+    se não estiver vazio, esconder o erro "E-mail é obrigatório"
+    verificar se o email é válido
+    se o email for válido, esconder o erro "E-mail é inválido"
+    se o email for inválido, mostrar o erro "E-mail é inválido"
+}
+
+function alternarErrosSenha() {
+    pegar o valor do campo de senha
+    verificar se o campo de senha está vazio
+    se estiver vazio, mostrar o erro "Senha é obrigatória"
+    se não estiver vazio, esconder o erro da senha
+}
+
+function alternarBotoesDesabilitados() {
+    verificar se o email é válido
+    se o email for válido, habilitar o botão de recuperar senha
+    se o email for inválido, desabilitar o botão de recuperar senha
+    verificar se a senha é válida
+    se o email e a senha forem válidos, habilitar o botão entrar
+    se o email ou a senha forem inválidos, desabilitar o botão entrar
+}
+
+------------------------------------------------------------------------------------------------------
+Estava assim com a lógica anterior, mas depois disso apliquei função de organização de código
+function alteracaoEmail(){
+    alternarBotoesDesabilitados();
+    alternarErrosEmail();
+}
+
+function alteracaoSenha() {
+    alternarBotoesDesabilitados();
+    alternarErrosSenha();
+}
+
+function descobreSeEmailValido() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        return false;
+    }
+        return validarEmail(email);
+}
+
+function descobreSeSenhaValida() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+        return false;
+     }
+      return true;
+ }
+
+function validarEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+ }
+ 
+function alternarErrosEmail() {
+    const email = document.getElementById("email").value;
+    if(!email){
+        document.getElementById("erro-email-obrigatorio").style.display = "block";
+    } else{
+        document.getElementById("erro-email-obrigatorio").style.display = "none";
+    }
+
+    if (validarEmail(email)) {
+        document.getElementById("erro-email-invalido").style.display = "none";
+    } else {
+        document.getElementById("erro-email-invalido").style.display = "block";
+    }
+}
+
+function alternarErrosSenha() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+        document.getElementById("erro-senha-obrigatoria").style.display = "block";
+    } else {
+        document.getElementById("erro-senha-obrigatoria").style.display = "none";
+    }
+}
+
+function alternarBotoesDesabilitados(){
+    const emailValido = descobreSeEmailValido();
+    document.getElementById('recover-password-button').disabled = !emailValido;
+
+    const senhaValida = descobreSeSenhaValida();
+    document.getElementById("login-button").disabled = !emailValido || !senhaValida;
+}
+----------------------------------------------------------------------------------------------------------
 
 */        
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function validarCampos(){
-    const emailValido = descobreSeEmailValido();
-    document.getElementById('recover-password-button').disabled = !emailValido;
+function alteracaoEmail(){
+    alternarBotoesDesabilitados();
+    alternarErrosEmail();
+}
 
-    const senhaValida = descobreSeSenhaValida();
-    document.getElementById("login-button").disabled = !emailValido || !senhaValida;
+function alteracaoSenha() {
+    alternarBotoesDesabilitados();
+    alternarErrosSenha();
 }
 
 function descobreSeEmailValido() {
@@ -26,3 +28,34 @@ function validarEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
  }
  
+function alternarErrosEmail() {
+    const email = document.getElementById("email").value;
+    if(!email){
+        document.getElementById("erro-email-obrigatorio").style.display = "block";
+    } else{
+        document.getElementById("erro-email-obrigatorio").style.display = "none";
+    }
+
+    if (validarEmail(email)) {
+        document.getElementById("erro-email-invalido").style.display = "none";
+    } else {
+        document.getElementById("erro-email-invalido").style.display = "block";
+    }
+}
+
+function alternarErrosSenha() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+        document.getElementById("erro-senha-obrigatoria").style.display = "block";
+    } else {
+        document.getElementById("erro-senha-obrigatoria").style.display = "none";
+    }
+}
+
+function alternarBotoesDesabilitados(){
+    const emailValido = descobreSeEmailValido();
+    document.getElementById('recover-password-button').disabled = !emailValido;
+
+    const senhaValida = descobreSeSenhaValida();
+    document.getElementById("login-button").disabled = !emailValido || !senhaValida;
+}
