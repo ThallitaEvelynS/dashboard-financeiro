555577a99ecf043189fbc4f99d4ef8fac92e9446
Thalita <evelynthalita55@gmail.com>
2026-09-02 19:43:21 -0300

    Adiciona conexão ao firebase e faz autenticação de email e senha para permitir entrada

--- index.js
+++ index.js
@@ -54,9 +54,33 @@ const form = {
 } 
 
 function login() {
-    window.location.href="pages/home/home.html";
+  firebase.auth().signInWithEmailAndPassword(
+    form.email().value, form.password().value
+    ).then(response => {
+        window.location.href="pages/home/home.html";
+    }).catch(error => {
+        alert(getMensagemErro(error));
+        console.log('error', error)
+    });
+}
+
+function getMensagemErro(error) {
+    if (error.code == "auth/invalid-credential") {
+        return "Usuário não encontrado";
+    }
+    return error.message;
 }
 
 function register() {
     window.location.href ="pages/register/register.html";
 }
+
+const firebaseConfig = {
+        apiKey: "AIzaSyAPbS5fB7IrBDaV6UaKSXrtLufRyNCZFPs",
+        authDomain: "controle-financeiro-41e5c.firebaseapp.com",
+        projectId: "controle-financeiro-41e5c",
+        storageBucket: "controle-financeiro-41e5c.firebasestorage.app",
+        messagingSenderId: "394921393017",
+        appId: "1:394921393017:web:63c5f0764bdd2badf38da8"
+  };
+  firebase.initializeApp(firebaseConfig);
